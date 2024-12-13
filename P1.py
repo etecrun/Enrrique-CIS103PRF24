49baaa6f6db93294f53aa2cf36092d34f4e518f7
etecrun <etecruceno@student.ccc.edu>
2024-12-12 18:45:37 -0600

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,12 @@
+Fname = input('Enter your first name')
+Lname = input('Enter your last name')
+Addy = input('Enter your home adress')
+City = input('What city do you live in?')
+State = input('What state do you live in?')
+Postal = input('What is your postal code?')
+Wname = Fname + ' ' + Lname
+Location = City + ', ' + State + ' ' + Postal
+print('\n')
+print(Wname)
+print(Addy)
+print(Location)
