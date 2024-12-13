871824a46e7b0c54dd621eebcb16f09bb5a9dea5
etecrun <etecruceno@student.ccc.edu>
2024-12-12 18:57:43 -0600

    Add files via upload

---
+++ MathP2.py
@@ -0,0 +1,17 @@
+import math
+Width = float( input('What is the width of the rectangle? '))
+Recheight = float( input('What is the height of the rectangle? '))
+Recarea = Width* Recheight
+print('\n')
+print('The area of the rectangle is :' , Recarea)
+print('\n')
+Tribase = float(input('What is the base of the triangle? '))
+Triheight = float(input('What is the height of the triangle? '))
+Triarea = 1/2 * Tribase * Triheight
+print('\n')
+print('The area of the triangle is :', Triarea)
+print('\n')
+Radius = float(input('What is the radius of the circle? '))
+Circarea = math.pi * Radius**2
+print('\n')
+print('The area of the circle is: ', Circarea)
